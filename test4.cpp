53f0ff0b4d095233309b89da123d042fc5ab6264
changzi67 <yuby17@mails.tsinghua.edu.cn>
2018-05-22 16:13:09 +0800

    update test4

--- test4.cpp
+++ test4.cpp
@@ -3,25 +3,27 @@
 #include "BaseNode.h"
 #include "Constant.h"
 #include "Placeholder.h"
-#include "Multiply.h"
+#include "Add.h"
 
 using namespace std;
 
 const int maxn = 3e5;
+//3e5秒出，5e5会爆栈
 BaseNode<float>* arr[maxn];
 
 int main(int argc, char const *argv[])
 {
 	auto a = Constant<float>(1);
 	auto x = Placeholder<float>("x");
-	arr[0] = new Multiply<float>(a,x);
-	arr[1] = new Multiply<float>(x,*arr[0]);
+	arr[0] = new Add<float>(a,x);
+	arr[1] = new Add<float>(x,*arr[0]);
 	for(int i=2;i<maxn;++i){
-		arr[i] = new Multiply<float>(arr[i-2],arr[i-1]);
+		arr[i] = new Add<float>(arr[i-2],arr[i-1]);
 	}
 	cout<<(double)clock()/CLOCKS_PER_SEC<<endl;
-	cout<<arr[maxn-1]->eval({{x,-1}})<<endl;
 
+	cout<<arr[maxn-1]->eval({{x,1}})<<endl;
+	
 	cout<<(double)clock()/CLOCKS_PER_SEC<<endl;
 	return 0;
 }
